22002f3092289c12a0f8f003614ec9d641cbd7a6
Francisco Quesada <fquesada@itba.edu.ar>
2022-03-18 02:03:34 -0300

    feat :sparkles: add benchmarks of both methods
    Co-authored-by: Gonzalo Arca <goarca@itba.edu.ar>
    Co-authored-by: Octavio Serpe <oserpe@itba.edu.ar>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,24 +16,33 @@ public class App {
 
 
     public static void main(String[] args) throws IOException {
-        run(STATIC_FILE_NAME, DYNAMIC_FILE_NAME, OUT_FILE_NAME, 10, 2, true);
-//        writeInputFiles(generateDistribution(500, 100, 1), "Static500.txt", "Dynamic500.txt", 100, 0);
+        double L = 1000;
+        double rc = 1;
+
+//        run(STATIC_FILE_NAME, DYNAMIC_FILE_NAME, OUT_FILE_NAME, 10, 2, false);
+        runBenchmark(1000, 1, rc, L,20000,0.25);
     }
 
-    public static void run(String staticPath, String dynamicPath, String outPath, int M, double rc, boolean isPeriodic) throws IOException {
-
-        StaticParameters staticParameters = readStaticParametersFromFile(staticPath);
-        DynamicParameters dynamicParameters = readDynamicParametersFromFile(dynamicPath, staticParameters.getParticleCount());
-
-        double L = staticParameters.getDim();
-
-        NeighbourDetection cellIndexMethod = new CellIndexMethod(L, M, rc,
-                createParticleList(dynamicParameters.getParticlePositionList(), staticParameters.getParticleRadiusList()));
-
+    private static Map<Long, List<Particle>> runBruteForce(int n, double rc, List<Particle> particles) {
+        NeighbourDetection bruteForceMethod = new BruteForce(n, rc, particles);
         Map<Long, List<Particle>> neighbourList;
 
-        long startTime = System.nanoTime();
+        long startTimeBrute = System.nanoTime();
 
+        neighbourList = bruteForceMethod.calculateNeighbourLists();
+
+        long endTimeBrute = System.nanoTime();
+
+        double millisBrute = ((double) (endTimeBrute - startTimeBrute)) / 1000000;
+        System.out.println("Elapsed time: " + millisBrute + " ms");
+        return neighbourList;
+    }
+
+    private static Map<Long, List<Particle>> runCellIndexMethod(double L, int M, double rc, List<Particle> particles, boolean isPeriodic) {
+        NeighbourDetection cellIndexMethod = new CellIndexMethod(L, M, rc, particles);
+
+        Map<Long, List<Particle>> neighbourList;
+        long startTime = System.nanoTime();
         if (isPeriodic) {
             neighbourList = cellIndexMethod.calculateNeighbourListsPeriodic();
         } else {
@@ -44,6 +53,40 @@ public class App {
 
         double millis = ((double) (endTime - startTime)) / 1000000;
         System.out.println("Elapsed time: " + millis + " ms");
+        return neighbourList;
+    }
+
+    public static void runBenchmark(int particleStep, int cellCountStep, double rc, double L, int maxParticleCount, double particleRadius) throws IOException {
+
+        writeInputFiles(generateDistribution(maxParticleCount, L, particleRadius), STATIC_FILE_NAME, DYNAMIC_FILE_NAME, L, 0);
+        StaticParameters staticParameters = readStaticParametersFromFile(STATIC_FILE_NAME);
+        DynamicParameters dynamicParameters = readDynamicParametersFromFile(DYNAMIC_FILE_NAME, staticParameters.getParticleCount());
+        List<Particle> particles = createParticleList(dynamicParameters.getParticlePositionList(), staticParameters.getParticleRadiusList());
+
+        int maxM = (int) Math.ceil(L / rc);
+        for (int n = particleStep; n < particles.size(); n += particleStep) {
+            List<Particle> sublist = particles.subList(0, n);
+            System.out.println("N = " + n + "=====================================");
+            System.out.print("Brute Force: ");
+            runBruteForce(n, rc, sublist);
+            for (int m = 1; m < maxM; m += cellCountStep) {
+                System.out.print("M = " + m + " (Periodic) ");
+                runCellIndexMethod(L, m, rc, sublist, true);
+                System.out.print("M = " + m + " ");
+                runCellIndexMethod(L, m, rc, sublist, false);
+            }
+        }
+    }
+
+
+    public static void run(String staticPath, String dynamicPath, String outPath, int M, double rc, boolean isPeriodic) throws IOException {
+
+        StaticParameters staticParameters = readStaticParametersFromFile(staticPath);
+        DynamicParameters dynamicParameters = readDynamicParametersFromFile(dynamicPath, staticParameters.getParticleCount());
+
+        double L = staticParameters.getDim();
+
+        Map<Long, List<Particle>> neighbourList = runCellIndexMethod(L, M, rc, createParticleList(dynamicParameters.getParticlePositionList(), staticParameters.getParticleRadiusList()), isPeriodic);
 
         writeNeighbourListToFile(neighbourList, outPath);
     }
@@ -99,17 +142,17 @@ public class App {
         return particleList;
     }
 
-    private static void writeInputFiles(List<Particle> particles, String staticPathname, String dynamicPathname, int L, int time) throws IOException {
+    private static void writeInputFiles(List<Particle> particles, String staticPathname, String dynamicPathname, double L, int time) throws IOException {
         PrintWriter staticPrintWriter = new PrintWriter(new FileWriter(staticPathname));
-        staticPrintWriter.printf("%d\n%d\n", particles.size(), L);
+        staticPrintWriter.printf("%d\n%.4f\n", particles.size(), L);
         PrintWriter dynamicPrintWriter = new PrintWriter(new FileWriter(dynamicPathname));
         dynamicPrintWriter.printf("%d\n", time);
         Position2D position2D;
-        for(int i = 0; i< particles.size();i++){
-            staticPrintWriter.printf("%.4f\t1.0000",particles.get(i).getRadius());
+        for (int i = 0; i < particles.size(); i++) {
+            staticPrintWriter.printf("%.4f\t1.0000", particles.get(i).getRadius());
             position2D = particles.get(i).getPosition();
-            dynamicPrintWriter.printf("%.7e\t%.7e",position2D.getX(),position2D.getY());
-            if( i != particles.size() - 1){
+            dynamicPrintWriter.printf("%.7e\t%.7e", position2D.getX(), position2D.getY());
+            if (i != particles.size() - 1) {
                 staticPrintWriter.println();
                 dynamicPrintWriter.println();
             }
@@ -119,8 +162,7 @@ public class App {
         dynamicPrintWriter.close();
     }
 
-    private static void writeNeighbourListToFile(Map<Long, List<Particle>> neighbourList, String fileName)
-            throws IOException {
+    private static void writeNeighbourListToFile(Map<Long, List<Particle>> neighbourList, String fileName) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName);
         PrintWriter printWriter = new PrintWriter(fileWriter);
 
@@ -133,15 +175,14 @@ public class App {
         printWriter.close();
     }
 
-    private static List<Particle> generateDistribution(long N, long L, double radius) {
-        double rightLimit = (double) L;
+    private static List<Particle> generateDistribution(long N, double L, double radius) {
 
         List<Particle> particles = new ArrayList<>();
         ;
 
         for (int i = 0; i < N; ) {
-            double xPosition = new Random().nextDouble() * rightLimit;
-            double yPosition = new Random().nextDouble() * rightLimit;
+            double xPosition = new Random().nextDouble() * L;
+            double yPosition = new Random().nextDouble() * L;
             Position2D position = new Position2D(xPosition, yPosition);
             boolean overlaps = false;
 
